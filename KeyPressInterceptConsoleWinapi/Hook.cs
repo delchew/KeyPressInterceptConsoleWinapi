9a5d702d3123692c41c8d59aacba2cfda3a58dba
delchew <delchew@gmail.com>
2022-08-21 21:58:38 +0300

    Add program

---
+++ KeyPressInterceptConsoleWinapi/Hook.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace KeyPressInterceptConsoleWinapi
+{
+	public class Hook : IDisposable
+	{
+        private delegate IntPtr KeyboardHookProc(int code, IntPtr wParam, IntPtr lParam);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WH_KEYDOWN = 0x0100;
+
+        // код клавиши на которую ставим хук
+        private int _key;
+        public event Action KeyPressed;
+
+        private readonly KeyboardHookProc _proc;
+        private IntPtr _hHook = IntPtr.Zero;
+
+        public Hook(int keyCode)
+        {
+            _key = keyCode;
+            _proc = HookProc;
+        }
+
+        public void SetHook()
+        {
+            var hInstance = LoadLibrary("User32");
+            _hHook = SetWindowsHookEx(WH_KEYBOARD_LL, _proc, hInstance, 0);
+        }
+
+        public void UnHook() => UnhookWindowsHookEx(_hHook);
+
+
+        public void Dispose() => UnHook();
+
+
+        private IntPtr HookProc(int code, IntPtr wParam, IntPtr lParam)
+        {
+            if (code >= 0 && wParam == (IntPtr)WH_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                KeyPressed?.Invoke();
+            }
+
+            // пробрасываем хук дальше
+            return CallNextHookEx(_hHook, code, (int)wParam, lParam);
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, KeyboardHookProc callback, IntPtr hInstance, uint threadId);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr idHook, int nCode, int wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnhookWindowsHookEx(IntPtr hInstance);
+    }
+}
--- KeyPressInterceptConsoleWinapi/Program.cs
+++ KeyPressInterceptConsoleWinapi/Program.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace KeyPressInterceptConsoleWinapi
 {
     class Program
     {
-        static void Main(string[] args)
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            keybd_event(0x90, 0x45, 0x1, (UIntPtr)0);
+
+            Console.WriteLine("Счетчик нажатия клавиши Caps Lock.\nДля завершения программы и вывода количества нажатий нажмите Esc.");
+
+            //https://docs.microsoft.com/ru-ru/windows/win32/inputdev/virtual-key-codes
+            int сapsLockKeyCode = 0x14;
+            int counter = 0;
+
+            using var hook = new Hook(сapsLockKeyCode);
+            hook.KeyPressed += () => counter++;
+            hook.SetHook();
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                if (keyInfo.Key == ConsoleKey.Escape)
+                    break;
+            }
+
+            Console.WriteLine("Клавиша Caps Lock была нажата {0} раз.", counter);
         }
     }
 }
-
